68109ab64e5afe45b6b0d603bf3290ee46df4496
Ian Damping <iandamping92@gmail.com>
2026-04-12 14:15:11 +0800

    feat(data): Create contract-based to get article data
    - Create ArticleService.kt for ArticleServiceImpl.kt to implement abstraction
    - Create ArticleRemoteDataSource.kt for ArticleRemoteDataSourceImpl.kt to implement abstraction
    - Create ArticleRepository.kt for ArticleRepositoryImpl.kt to implement abstraction

---
+++ shared/src/commonMain/kotlin/com/junemon/multiplatform_masterclass/articles/data/ArticleRemoteDataSourceImpl.kt
@@ -0,0 +1,17 @@
+package com.junemon.multiplatform_masterclass.articles.data
+
+import com.junemon.multiplatform_masterclass.articles.common.DataResult
+import com.junemon.multiplatform_masterclass.articles.model.ApiResult
+import com.junemon.multiplatform_masterclass.articles.model.ArticleData
+import com.junemon.multiplatform_masterclass.articles.model.ArticlesResponse
+import com.junemon.multiplatform_masterclass.articles.service.ArticleService
+
+class ArticleRemoteDataSourceImpl(private val service: ArticleService) : ArticleRemoteDataSource {
+
+    override suspend fun getArticles(): DataResult<List<ArticleData>> {
+        return when (val remoteData = service.fetchArticles()) {
+            is ApiResult.Error -> DataResult.Error(remoteData.message)
+            is ApiResult.Success<ArticlesResponse> -> DataResult.Data(remoteData.data.articles)
+        }
+    }
+}
