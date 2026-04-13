e1f289c5ef808ccc49579d3047781d364055b29a
Ian Damping <iandamping92@gmail.com>
2026-04-13 14:04:38 +0800

    feat(data): Create Local Data Source
    - Create ArticleLocalDataSource to abstract database use
    - Wiring ArticleRepositoryImpl.kt to use data from local and remote

--- shared/src/commonMain/kotlin/com/junemon/multiplatform_masterclass/core/data/articles/repository/ArticleRepositoryImpl.kt
+++ shared/src/commonMain/kotlin/com/junemon/multiplatform_masterclass/core/data/articles/repository/ArticleRepositoryImpl.kt
@@ -11,42 +11,66 @@ import kotlinx.datetime.TimeZone
 import kotlinx.datetime.daysUntil
 import kotlinx.datetime.toLocalDateTime
 import kotlinx.datetime.todayIn
+import kotlin.time.Duration.Companion.hours
 
 class ArticleRepositoryImpl(
     private val remoteData: ArticleRemoteDataSource,
     private val localData: ArticleLocalDataSource
 ) : ArticleRepository {
 
-    override suspend fun getArticles(): DataResult<List<Article>> {
-        return when (val remoteData = remoteData.getArticles()) {
-            is DataResult.Data<List<ArticleData>> -> DataResult.Data(remoteData.data.map { article ->
-                Article(
-                    title = article.title,
-                    description = article.desc ?: "Click to find out more",
-                    date = getDaysAgoString(article.date),
-                    imageUrl = article.imageUrl
-                        ?: "https://image.cnbcfm.com/api/v1/image/107326078-1698758530118-gettyimages-1765623456-wall26362_igj6ehhp.jpeg?v=1698758587&w=1920&h=1080"
-                )
-            })
+    private val databaseTimeout = 24.hours.inWholeMilliseconds
 
-            is DataResult.Error -> DataResult.Error(remoteData.message)
+    private fun isLocalDataExpired(): Boolean {
+        val count = localData.getArticlesCount()
+        val lastSync = localData.getLastUpdate()
+
+        return count == 0L || (Clock.System.now()
+            .toEpochMilliseconds() - lastSync) > databaseTimeout
+    }
+
+    override suspend fun getArticles(): DataResult<List<Article>> {
+        return if (isLocalDataExpired()) {
+            when (val remoteData = remoteData.getArticles()) {
+                is DataResult.Data<List<ArticleData>> -> {
+                    localData.insertArticles(remoteData.data)
+                    localData.setLastUpdate(Clock.System.now().toEpochMilliseconds())
+
+                    DataResult.Data(articlesMapper(localData.getArticles()))
+                }
+
+                is DataResult.Error -> DataResult.Error(remoteData.message)
+            }
+        } else {
+            DataResult.Data(articlesMapper(localData.getArticles()))
+        }
+    }
+
+    private fun articlesMapper(rawData: List<ArticleData>): List<Article> {
+        return rawData.map { article ->
+            Article(
+                title = article.title,
+                description = article.desc ?: "Click to find out more",
+                date = getDaysAgoString(article.date),
+                imageUrl = article.imageUrl
+                    ?: "https://image.cnbcfm.com/api/v1/image/107326078-1698758530118-gettyimages-1765623456-wall26362_igj6ehhp.jpeg?v=1698758587&w=1920&h=1080"
+            )
         }
     }
 
     private fun getDaysAgoString(date: String): String {
         val systemTZ = TimeZone.currentSystemDefault()
 
-        // 1. Ambil tanggal hari ini berdasarkan zona waktu sistem
+        // 1.Get today's date based on system time zone
         val today = Clock.System.todayIn(systemTZ)
 
-        // 2. Parsing input string ke Instant, lalu konversi ke LocalDate
-        // Pastikan string input dalam format ISO 8601 (contoh: 2023-10-27T10:00:00Z)
+        // 2.Parse the input string to Instant, then convert to LocalDate, M
+        // Ensure the input string is in ISO 8601 format (e.g., 2023-10-27T10:00:00Z).
         val targetDate = Instant.parse(date)
             .toLocalDateTime(systemTZ)
             .date
 
-        // 3. Hitung selisih hari (today - targetDate)
-        // daysUntil menghasilkan nilai negatif jika targetDate di masa lalu
+        // 3.Calculate the difference in days (today - targetDate)
+        // daysUntil returns a negative value if the target Date is in the past.
         val days = targetDate.daysUntil(today)
 
         return when {
@@ -57,3 +81,4 @@ class ArticleRepositoryImpl(
         }
     }
 }
+
